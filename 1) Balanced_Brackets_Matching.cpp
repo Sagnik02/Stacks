c0490358a187b48f3a7b2a12f4749e246827e2a1
Sagnik Roy Chaudhury <108208974+Sagnik02@users.noreply.github.com>
2023-05-06 21:33:31 +0530

    Add files via upload

---
+++ 1) Balanced_Brackets_Matching.cpp
@@ -0,0 +1,122 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+class Stack {
+    //properties
+    public:
+        int *arr;
+        int top;
+        int size;
+
+    // behaviour
+    Stack(int size) {
+        this -> size = size;
+        arr = new int[size];
+        top = -1;
+    }
+
+    void push( int element) {
+        if(size - top > 1) {
+            top++;
+            arr[top] = element;
+        }
+        else{
+            cout << "Stack OverFlow" << endl;
+        }
+    }
+
+    void pop() {
+        if(top >=0 ) {
+            top--;
+        }
+        else{
+            cout << "Stack UnderFlow" << endl;
+        }
+    }
+
+    int peek() {
+        if(top >=0 )
+            return arr[top];
+        else
+        {
+            cout << "Stack is Empty" << endl;
+            return -1;
+        }
+    }
+
+    bool isEmpty() {
+        if( top == -1) {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+};
+
+
+int main() {
+
+
+    Stack st(6);
+
+    st.push(20);
+    st.push(40);
+    st.push(50);
+    st.push(60);
+    st.push(70);
+    st.push(80);
+
+    cout << st.peek() << endl;
+
+    st.pop();
+
+    cout << st.peek() << endl;
+
+    st.pop();
+
+    cout << st.peek() << endl;
+
+    st.pop();
+
+    cout << st.peek() << endl;
+    st.pop();
+
+    cout << st.peek() << endl;
+    st.pop();
+
+    cout << st.peek() << endl;
+    st.pop();
+
+    cout << st.peek() << endl;
+    
+    if(st.isEmpty()) {
+        cout << "Stack is Empty " << endl;
+    }
+    else{
+        cout << "Stack is not Empty " << endl;
+    }
+
+    
+    /*
+    //creation of stack
+    stack<int> s;
+    //push operation
+    s.push(2);
+    s.push(3);
+    //pop
+    s.pop();
+    cout << "Printing top element " << s.top() << endl;
+    if(s.empty()){
+        cout << "Stack is empty " << endl;
+    }
+    else{
+        cout << "stack is not empty " << endl;
+    }
+    cout << "size of stack is " << s.size() << endl;
+    */
+
+    return 0;
+}
